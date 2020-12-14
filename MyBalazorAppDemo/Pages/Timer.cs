2d3881d5af6d4da9e0d2ae933ba4c4c42b348e2e
Amit Dubey <amit.dubey@onsolve.com>
2020-12-15 02:19:27 +0530

    Progress commit

---
+++ MyBalazorAppDemo/Pages/Timer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YoYoTest.UI.Pages;
+
+namespace YoYoTest.UI.Pages
+{
+    public partial class Timer
+    {
+    }
+}
--- YoYoTest.App/Program.cs
+++ YoYoTest.App/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace YoYoTest.UI
+namespace YoYoTest.App
 {
     public class Program
     {
